6b5ee13d1a36f5be414eb0da528109750b212c60
Stefan Urech <stefan@surech.ch>
2015-12-05 18:10:03 +0100

    stu: Ich kann einen QR-Code lesen!

--- src/main/java/ch/poinzofnoreturn/qrcode/rest/QrCodeScanner.java
+++ src/main/java/ch/poinzofnoreturn/qrcode/rest/QrCodeScanner.java
@@ -1,8 +1,15 @@
 package ch.poinzofnoreturn.qrcode.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.imageio.ImageIO;
+import javax.ws.rs.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * REST-Schnittstelle für das Erkennen eines QR-Code
@@ -10,10 +17,18 @@ import javax.ws.rs.Path;
 @Path("/qrcode/scanner")
 public class QrCodeScanner {
 
-    @GET
+    @POST
     @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    @Consumes("*/*")
+    public String scan(InputStream input) {
+
+        try {
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(input))));
+            MultiFormatReader reader = new MultiFormatReader();
+            Result result = reader.decode(bitmap);
+            return result.getText();
+        } catch (Exception e) {
+            return "Fehler: " + e;
+        }
     }
 }
